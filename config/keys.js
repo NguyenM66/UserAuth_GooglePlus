2934037f25e6807428420834185dfc478a2c3f22
Monica <monica.nguyen@gmail.com>
2018-01-06 01:20:08 -0500

    complete basic user authentication for google+

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+// add this file to .gitignore
+
+module.exports = {
+	google: {
+		clientID: '998739223179-umgooib6lj4iprha720ish8gijb152ed.apps.googleusercontent.com',
+		clientSecret: 'Ub4pDiYVrb4jvbv2H_MqzuN4',
+	},
+	mongodb: {
+		dbURI: 'mongodb://NguyenM66:monica66@ds239557.mlab.com:39557/tripwise_oauth'
+	},
+	session: {
+		cookieKey: 'tripwise is awesome'
+	}
+}
